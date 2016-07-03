36047f5717507507488fa843a89017678d875150
YONATAN <yonarbel@gmail.com>
2016-07-03 19:44:06 +0300

    injecting data service - fake server with in memeory backend service

---
+++ src/app/in-memory-data.service.js
@@ -0,0 +1,4 @@
+/**
+ * Created by yonarbel on 7/3/16.
+ */
+//# sourceMappingURL=in-memory-data.service.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+// Imports for loading & configuring the in-memory web api
+import { XHRBackend } from '@angular/http';
+
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }               from './app/in-memory-data.service';
+
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
 import { APP_ROUTER_PROVIDERS } from './app/app.routes';
 import { HTTP_PROVIDERS } from '@angular/http';
+
+
 if (environment.production) {
   enableProdMode();
 }
